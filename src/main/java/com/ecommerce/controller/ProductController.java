880ef4503ff037e2bd8651ed142d25429450e512
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-27 03:12:13 +0100

    ECommerce

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.datagenerator.DataGenerator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,9 @@ public class ProductController {
     }
 
     @GetMapping("/list")
-    public String getProductList(){
+    public String getProductList(Model model){
+
+        model.addAttribute("products", DataGenerator.getProductList());
 
         return "/product/productList";
 
